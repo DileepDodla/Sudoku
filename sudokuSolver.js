45895f2eff9999e4a6f2b2f70cbd6919c59b43d9
DileepDodla <dileepreddy1999@gmail.com>
2019-02-04 22:25:09 +0530

    Update sudokuSolver.js

--- sudokuSolver.js
+++ sudokuSolver.js
@@ -1,25 +1,25 @@
-    var sudokuArray = [
-            [5,3,null,null,7,null,null,null,null],//[5,3, , ,7, , , , ]
-            [6,null,null,1,9,5,null,null,null],
-            [null,9,8,null,null,null,null,6,null],
-            [8,null,null,null,6,null,null,null,3],
-            [4,null,null,8,null,3,null,null,1],
-            [7,null,null,null,2,null,null,null,6],
-            [null,6,null,null,null,null,2,8,null],
-            [null,null,null,4,1,9,null,null,5],
-            [null,null,null,null,8,null,null,7,9]
-    ];
     // var sudokuArray = [
-    //         [5,3,4,6,7,8,9,1,2],
-    //         [6,7,2,1,9,5,3,4,8],
-    //         [1,9,8,3,4,2,5,6,7],
-    //         [8,5,9,7,6,1,4,2,3],
-    //         [4,2,6,8,5,3,7,9,1],
-    //         [7,1,3,9,2,4,null,5,6],
-    //         [9,6,1,5,3,7,2,8,4],
-    //         [2,8,7,4,1,9,6,3,5],
-    //         [3,4,5,2,8,6,1,7,9]
+    //         [5,3,null,null,7,null,null,null,null],//[5,3, , ,7, , , , ]
+    //         [6,null,null,1,9,5,null,null,null],
+    //         [null,9,8,null,null,null,null,6,null],
+    //         [8,null,null,null,6,null,null,null,3],
+    //         [4,null,null,8,null,3,null,null,1],
+    //         [7,null,null,null,2,null,null,null,6],
+    //         [null,6,null,null,null,null,2,8,null],
+    //         [null,null,null,4,1,9,null,null,5],
+    //         [null,null,null,null,8,null,null,7,9]
     // ];
+    var sudokuArray = [
+            [5,3,4,6,7,8,9,1,2],
+            [6,7,2,1,9,5,3,4,8],
+            [1,9,8,3,4,2,5,6,7],
+            [8,5,9,7,6,1,4,2,3],
+            [4,2,6,8,5,3,7,9,1],
+            [7,1,3,9,2,4,null,5,6],
+            [9,6,1,5,3,7,2,8,4],
+            [2,8,7,4,1,9,6,3,5],
+            [3,4,5,2,8,6,1,7,9]
+    ];
 
     for(var i=0;i<9;i++){
         for(var j=0;j<9;j++){
